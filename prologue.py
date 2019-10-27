1d74d229dc592e33206a70ea7c97ff7b920a315f
Martin Borchert <martin-anon@posteo.de>
2019-10-27 16:18:43 +0100

    Add a sandbox-like Python wrapper

---
+++ prologue.py
@@ -0,0 +1,9 @@
+# Don't forget to unimport as much as possbile afterwards.
+import sys
+
+# Disable Traceback to make fake name errors
+# seem like the real ones
+sys.tracebacklimit = 0
+
+# Unimport!
+del sys
