a568f9ef9a43b9dded07fa0c5f596f6095bb49a7
Konrad Nölle <konrad@familie-noelle.de>
2019-12-11 17:46:54 +0100

    Truncate long stdout

--- epilogue.py
+++ epilogue.py
@@ -1,5 +1,5 @@
 import json
 import sys
 
-__app_response__["stdout"] = sys.stdout.getvalue()
+__app_response__["stdout"] = sys.stdout.getvalue()[:100000]
 __original_stdout__.write(json.dumps(__app_response__))
