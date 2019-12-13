7d7aef6d2b93860b026e068ee30beb326ad4a1f6
Martin Borchert <martin-anon@posteo.de>
2019-12-13 21:28:06 +0100

    Revert "Truncate long stdout"
    This reverts commit a568f9ef9a43b9dded07fa0c5f596f6095bb49a7.

--- epilogue.py
+++ epilogue.py
@@ -1,5 +1,5 @@
 import json
 import sys
 
-__app_response__["stdout"] = sys.stdout.getvalue()[:100000]
+__app_response__["stdout"] = sys.stdout.getvalue()
 __original_stdout__.write(json.dumps(__app_response__))
