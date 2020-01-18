f51f83f095d282f39a00499fa8186318b68632e8
Hendrik <dev.nipe.systems@gmail.com>
2020-01-18 20:39:01 +0100

    Refactor and let's stop the emoji game...

--- html-finalizer.py
+++ html-finalizer.py
@@ -6,6 +6,31 @@ import bs4
 
 html_path = sys.argv[1]
 
+def generate_viewer(soup, attributes):
+  paragraph = soup.new_tag('p')
+  b = soup.new_tag('b')
+  b.string = 'Viewer'
+  paragraph.append(b)
+  paragraph.append(' of type ')
+  code = soup.new_tag('code')
+  code.string = attributes['type']
+  paragraph.append(code)
+  paragraph.append(' for ')
+  code = soup.new_tag('code')
+  code.string = attributes['file']
+  paragraph.append(code)
+  return paragraph
+
+def generate_editor(soup, attributes):
+  new_tag = soup.new_tag('b')
+  new_tag.string = f'Editor of type \'{attributes["type"]}\' for \'{attributes["file"]}\''
+  return new_tag
+
+def generate_terminal(soup, attributes):
+  new_tag = soup.new_tag('b')
+  new_tag.string = f'Terminal in initial working-directory \'{attributes["working-directory"]}\''
+  return new_tag
+
 for root, dirs, files in os.walk(html_path):
   for file in [file for file in files if os.path.splitext(file)[1] == '.html']:
     file_path = os.path.join(root, file)
@@ -13,16 +38,10 @@ for root, dirs, files in os.walk(html_path):
     with open(file_path) as f:
       soup = bs4.BeautifulSoup(f.read(), 'html.parser')
       for viewer in soup.find_all('x-viewer'):
-        new_tag = soup.new_tag('b')
-        new_tag.string = f'Viewer of type \'{viewer["type"]}\' for \'{viewer["file"]}\''
-        viewer.replace_with(new_tag)
+        viewer.replace_with(generate_viewer(soup, viewer))
       for editor in soup.find_all('x-editor'):
-        new_tag = soup.new_tag('b')
-        new_tag.string = f'Editor of type \'{editor["type"]}\' for \'{editor["file"]}\''
-        editor.replace_with(new_tag)
+        editor.replace_with(generate_editor(soup, editor))
       for terminal in soup.find_all('x-terminal'):
-        new_tag = soup.new_tag('b')
-        new_tag.string = f'Terminal in initial working-directory \'{terminal["working-directory"]}\''
-        terminal.replace_with(new_tag)
+        terminal.replace_with(generate_terminal(soup, terminal))
     with open(file_path, 'w') as f:
       f.write(soup.encode(formatter='html5').decode('utf-8'))
