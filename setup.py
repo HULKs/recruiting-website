41a390a9b25a4f20b86d911f8137bace35230e99
Hendrik <git@h3ndrk.de>
2021-01-28 21:29:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ setuptools.setup(
         'mkdocs>=1.1.2',
     ],
     entry_points={
+        'mkdocs.plugins': [
+            'recruiting_website_navigation = recruiting_website.navigation.navigation:Plugin',
+        ],
         'mkdocs.themes': [
-            'recruiting_website = theme',
+            'recruiting_website = recruiting_website.theme',
         ],
     },
     include_package_data=True,
